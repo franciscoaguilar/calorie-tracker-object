057f1093db6cc3a29751460f549d01d6ad52f083
francisco <0667034@my.scccd.edu>
2018-08-17 19:54:55 -0700

    index ready

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const meal = {
+  _mealArray: [],
+addMeal(food, calories){
+  let meal = {
+  food: food,
+  calories: calories,
+ }
+ this._mealArray.push(meal);
+},
+// get meal(){
+//   return this._mealArray
+// },
+
+}
